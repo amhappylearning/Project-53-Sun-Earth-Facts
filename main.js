b6a54c2a0f85cce380deaa118b0a1bd53412ccbf
AM <73906893+amhappylearning@users.noreply.github.com>
2021-04-29 10:14:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function change_color_blue(){
+    document.getElementById("b1").innerHTML.style.color = "blue"; 
+  }
+  
+  flipimage(){
+    document.getElementById("image1").src = "Season Sunrise.jpeg";
+  }
+  
